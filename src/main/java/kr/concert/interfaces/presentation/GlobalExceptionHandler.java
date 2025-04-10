416b5f537daa8ca9a5d78541a9be9aa55bc3f4b2
kimyezzang97 <kimyezzang97@gmail.com>
2025-04-10 21:22:48 +0900

    feat: MemberException 설정

---
+++ src/main/java/kr/concert/interfaces/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package kr.concert.interfaces.presentation;
+
+import kr.concert.interfaces.member.MemberException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice // 전역설정을 위한 어노테이션
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MemberException.MemberNotFoundException.class)
+    public ApiResponse<?> memberNotFound(MemberException.MemberNotFoundException e) {
+         return new ApiResponse<>(false, 404, e.getMessage(), null);
+    }
+
+    @ExceptionHandler(MemberException.CanNotTooMuchChargeException.class)
+    public ApiResponse<?> CanNotTooMuchChargeException(MemberException.CanNotTooMuchChargeException e) {
+        return new ApiResponse<>(false, 400, e.getMessage(), null);
+    }
+
+    @ExceptionHandler(MemberException.CanNotMinusChargeException.class)
+    public ApiResponse<?> CanNotMinusChargeException(MemberException.CanNotMinusChargeException e) {
+        return new ApiResponse<>(false, 400, e.getMessage(), null);
+    }
+
+}
