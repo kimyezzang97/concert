9e0f932b03f3c3c08ed7736bdc6beb9539cfba05
kimyezzang97 <kimyezzang97@gmail.com>
2025-04-15 00:20:19 +0900

    fix: JpaConfig 설정 어노테이션 주석 처리

--- src/main/java/kr/concert/infra/config/jpa/JpaConfig.java
+++ src/main/java/kr/concert/infra/config/jpa/JpaConfig.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories
+// @EnableJpaRepositories() ㅡㅡ^
 public class JpaConfig {
 
     @Bean
