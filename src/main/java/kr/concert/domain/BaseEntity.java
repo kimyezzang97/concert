e6a34f6778fb20c96d690840be7207cb94101016
kimyezzang97 <kimyezzang97@gmail.com>
2025-04-15 22:42:35 +0900

    fix: BaseEntity @MappedSuperclass추가

--- src/main/java/kr/concert/domain/BaseEntity.java
+++ src/main/java/kr/concert/domain/BaseEntity.java
@@ -2,22 +2,25 @@ package kr.concert.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
