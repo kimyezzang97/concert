b2c17e1b08796cca3527064bd2f9af5366925d37
kimyezzang97 <kimyezzang97@gmail.com>
2025-04-17 01:51:02 +0900

    test: Queue 토큰 대기열 정보 조회 service 단위 테스트 구현

--- src/test/java/kr/concert/domain/queue/QueueServiceTest.java
+++ src/test/java/kr/concert/domain/queue/QueueServiceTest.java
@@ -1,6 +1,8 @@
 package kr.concert.domain.queue;
 
 import kr.concert.domain.member.Member;
+import kr.concert.interfaces.queue.QueueException;
+import kr.concert.interfaces.queue.QueueResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -55,4 +59,44 @@ class QueueServiceTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("회원 정보는 필수입니다."); // 예외 메시지까지 검증
     }
+
+    @DisplayName("존재하지 않는 토큰이면 예외를 던집니다.")
+    @Test
+    void getQueueStatusWithInvalidTokenThrowsException() {
+        // given
+        String invalidToken = UUID.randomUUID().toString();
+        when(queueRepository.findByToken(invalidToken)).thenReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> queueService.getQueue(invalidToken))
+                .isInstanceOf(QueueException.TokenNotExistException.class)
+                .hasMessage("Token Not Exists");
+    }
+
+    @DisplayName("유효한 토큰으로 대기열 정보를 조회합니다.")
+    @Test
+    void getQueueStatusWithValidToken() {
+        // given
+        Member member = new Member(1L, "김예찬", 1000L);
+        String token = UUID.randomUUID().toString();
+
+        Queue queue = new Queue(
+                5L, // queueId
+                member,
+                token,
+                QueueStatus.WAIT,
+                null
+        );
+
+        when(queueRepository.findByToken(token)).thenReturn(Optional.of(queue));
+        when(queueRepository.countByQueueStatusAndQueueIdLessThan(QueueStatus.WAIT, 5L)).thenReturn(4L);
+
+        // when
+        QueueResponse.QueueStatus result = queueService.getQueue(token);
+
+        // then
+        assertThat(result.status()).isEqualTo("WAIT");
+        assertThat(result.position()).isEqualTo(5); // 4명 앞 + 자기 자신
+    }
+
 }
